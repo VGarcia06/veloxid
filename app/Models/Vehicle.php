9c5f152c09231caea7ee1cfa23985bbff3305902
MarietaMS <shugomar24@gmail.com>
2020-10-20 23:07:22 -0500

    COMMIT VEHICULOS

---
+++ app/Models/Vehicle.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Vehicle extends Model
+{
+        /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'placa',
+        'capacidadCarga',
+        'imagen',
+        'idDriver',
+        'idVehicleType'
+    ];
+}
