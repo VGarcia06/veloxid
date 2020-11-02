fef0e8535923e9a8410250db5ae0c97326efad33
Lugonis018 <lugonis@outlook.com>
2020-11-02 01:10:32 -0500

    Adding Vehicle Controller functions get and store

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -3,19 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Models\Vehicle;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class VehicleController extends Controller
 {
     
-    public function index()
+    public function index($id)
     {
-        $vehicle = Vehicle::all();
+        $vehicles = User::find($id)
+                                ->first()
+                                ->driver()
+                                ->first()
+                                ->vehicles()
+                                ->paginate(15);
 
-        return $vehicle;
+        return response()->json([
+            'message' => True,
+            'vehicles' => $vehicles
+        ], 200);
     }
 
     function show($id)
@@ -23,14 +33,49 @@ class VehicleController extends Controller
         
     }
 
-    public function create()
+    public function store(Request $request, $id)
     {
+
+        try {
+            $user = User::find($id);
+
+            if ($request->hasFile('imagen')) {
+                $request->imagen = Storage::put('vehicles', $request->file('imagen'), 'public');
+            }
+
+            $user->driver()
+                    ->first()
+                    ->vehicles()
+                    ->createMany([
+                        [
+                            'placa' => $request->placa,
+                            'capacidadCarga' => $request->capacidadCarga,
+                            'imagen' => $request->imagen,
+                            'idVehicleType' => $request->idVehicleType,
+                        ]
+                    ]);
+
+        } catch (\Throwable $th) {
+            //throw $th;
+
+            return response()->json([
+                'message' => False
+            ], 400);
+        }
        
+        return response()->json([
+            'message' => True,
+        ], 201);
     }
 
     // store...
 
-    public function edit($id)
+    public function update($id)
+    {
+        
+    }
+
+    public function destroy($id)
     {
         
     }
