a731a01db43f4a10802a539e72cdc9b3cf080fc3
Lugonis018 <lugonis@outlook.com>
2020-11-13 00:58:28 -0500

    Updating VehicleEvaluationController to add all requirement by back-end

--- app/Http/Controllers/VehicleEvaluationController.php
+++ app/Http/Controllers/VehicleEvaluationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Vehicle;
+use App\Models\VehicleRequirement;
 use Illuminate\Support\Facades\DB;
 
 class VehicleEvaluationController extends Controller
@@ -30,13 +31,30 @@ class VehicleEvaluationController extends Controller
             DB::beginTransaction();
             $vehicle = Vehicle::find($id);
 
+            $vehicle_requirements = VehicleRequirement::all();
+            
+            $suitable = 1;
+
+            $id_requirements = [];
+
+            foreach ($request->evals as $eval) {
+                $id_requirements[] = $eval['idRequirement'];
+            }
+
+            if ($vehicle_requirements->except($id_requirements) !== null) {
+                $suitable = 2;
+            }
+
+
+
+            /*
             $suitable = 1;
             foreach ($request->evals as $eval) {
                 if (!$eval['valor']) {
                     $suitable = 2;
                 }
             }
-
+            */
             $revision = $vehicle->revisions()
                                 ->createMany([
                                     [
@@ -45,11 +63,22 @@ class VehicleEvaluationController extends Controller
                                     ]
                                 ])
                                 ->first();
+            foreach ($vehicle_requirements as $vehicle_requirement) {
+                if (in_array($vehicle_requirement['id'], $id_requirements)) {
+                    $revision->requirements()
+                        ->attach($vehicle_requirement['id'], ['valor' => 1]);
+                }else {
+                    $revision->requirements()
+                        ->attach($vehicle_requirement['id'], ['valor' => 0]);
+                }
+                
+            }
+            /*
             foreach ($request->evals as $eval) {
                 $revision->requirements()
                         ->attach($eval['idRequirement'], ['valor' => $eval['valor']]);
             }
-
+            */
             DB::commit();
             
         } catch (\Throwable $th) {
